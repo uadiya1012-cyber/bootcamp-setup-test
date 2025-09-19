a7b9bd18cb31667a52a41664abd52406bf667eff
athena <athena@ADMIN-08s-iMac.local>
2025-09-19 12:50:22 +0800

    add day 05'

---
+++ favorites.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read -p "Таны дуртай өнгө юу вэ? " FAVORITE_COLOR
+read -p "Таны дуртай хоол юу вэ? " FAVORITE_FOOD
+echo "Таны дуртай өнгө бол $FAVORITE_COLOR, харин дуртай хоол чинь $FAVORITE_FOOD юм байна."
---
+++ file_checker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -f "test.txt" ]; then
+  echo "test.txt файл олдлоо."
+else
+  echo "test.txt файл олдсонгүй."
+fi
